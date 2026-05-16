f2a9861936edda5d734c81c7df41e258bb2779fa
Alexey Kozadaev <alexey.kozadaev@softezza.ru>
2026-05-16 14:07:58 +0300

    добавляет/обновляет клиента

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -3,11 +3,12 @@ package main
 import (
 	"context"
 	"fmt"
-	"github.com/akozadaev/grpc-demo/echo"
-	helper "github.com/akozadaev/grpc-demo/pkg"
 	"log"
 	"os"
 	"time"
+
+	"github.com/akozadaev/grpc-demo/echo"
+	helper "github.com/akozadaev/grpc-demo/pkg"
 
 	"google.golang.org/grpc"
 )
@@ -16,7 +17,7 @@ func main() {
 	address := helper.GetEnv("PORT")
 	target := fmt.Sprintf("localhost:%s", address)
 
-	conn, err := grpc.Dial(target, grpc.WithInsecure(), grpc.WithBlock())
+	conn, err := grpc.NewClient(target, grpc.WithInsecure(), grpc.WithBlock())
 	if err != nil {
 		log.Fatalf("не удалось подключиться: %v", err)
 	}
@@ -24,6 +25,7 @@ func main() {
 	defer conn.Close()
 
 	c := echo.NewEchoServiceClient(conn)
+	cm := echo.NewMathServiceClient(conn)
 
 	msg := "Hello, gRPC!"
 	if len(os.Args) > 1 {
@@ -38,4 +40,26 @@ func main() {
 		log.Fatalf("ошибка вызова Echo: %v", err)
 	}
 	log.Printf("Ответ: %s", r.Message)
+
+	a, err := cm.Add(ctx, &echo.NumbersRequest{A: 3, B: 1})
+	log.Printf("Сумма: %s", a)
+
+	d, err := cm.Divide(ctx, &echo.NumbersRequest{A: 3, B: 1})
+	if err != nil {
+		log.Fatalf("ошибка: %v", err)
+	}
+	log.Printf("Деление: %s", d)
+
+	m, err := cm.Multiply(ctx, &echo.NumbersRequest{A: 3, B: 1})
+	if err != nil {
+		log.Fatalf("ошибка: %v", err)
+	}
+	log.Printf("Умножение: %s", m)
+
+	s, err := cm.Subtract(ctx, &echo.NumbersRequest{A: 3, B: 1})
+	if err != nil {
+		log.Fatalf("ошибка: %v", err)
+	}
+	log.Printf("Вычитание: %s", s)
+
 }
